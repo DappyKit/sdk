2bd18e602a4ffc4a7550f71c4c267a0e4a1a9058
Igor Shadurin <igor.shadurin@gmail.com>
2024-03-26 14:24:55 +0300

    test: upgrade browser tests

--- test/manual/script.js
+++ test/manual/script.js
@@ -4,6 +4,8 @@ const sdk = new window.DappyKit.SDK(
 )
 
 ;(async () => {
+  const verificationContractAddress = '0x721462E34DCC00F8Bd0f0cD07762cfd482a0Fcb4'
+
   // eslint-disable-next-line no-console
   console.log('Smart Account address', await sdk.account.getAddress())
 
@@ -29,5 +31,23 @@ const sdk = new window.DappyKit.SDK(
   console.log(
     'service filesystem multihash',
     await sdk.filesystemChanges.getServiceChangeMultihash('0x14d435d97ccf8Fc8da9B9364bFD5E91729502a0b'),
+  )
+
+  // eslint-disable-next-line no-console
+  console.log(
+    'verification check (expect true)',
+    await sdk.verification.getIsVerified('0x293AC5D2C7df3e9742d8e562d7cEE8Ba6ce43D81', verificationContractAddress),
+  )
+
+  // eslint-disable-next-line no-console
+  console.log(
+    'verification check (expect false)',
+    await sdk.verification.getIsVerified('0x3a6019cc36FB715Bd7aE6f83BEA98Bbe77E06f50', verificationContractAddress),
+  )
+
+  // eslint-disable-next-line no-console
+  console.log(
+    'verification check (expect 68579410290623560541334757137125908201458771167157024679308233665838308954395)',
+    await sdk.verification.getTokenId('0x293AC5D2C7df3e9742d8e562d7cEE8Ba6ce43D81', verificationContractAddress),
   )
 })()
