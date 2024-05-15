944e35dba000272dadfd6301cbb77b81de24a093
Igor Shadurin <igor.shadurin@gmail.com>
2024-05-15 18:26:53 +0100

    feat: ok signer converter

--- src/utils/signer.ts
+++ src/utils/signer.ts
@@ -1,11 +1,18 @@
-import { HDAccount } from 'viem/accounts'
 import { ISigner } from '../service/delegated-fs/interfaces'
+import type { Hash, SignableMessage } from 'viem/types/misc'
+
+/**
+ * Account signer interface.
+ */
+export interface IAccountSigner {
+  signMessage: ({ message }: { message: SignableMessage }) => Promise<Hash>
+}
 
 /**
  * Converts an account to a signer.
  * @param account HDAccount from the viem/accounts package
  */
-export function accountToSigner(account: HDAccount): ISigner {
+export function accountToSigner(account: IAccountSigner): ISigner {
   return {
     signMessage: async (message: string | Uint8Array): Promise<string> => {
       const data = typeof message === 'string' ? { message } : { message: { raw: message } }
