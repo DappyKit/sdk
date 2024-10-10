f0a802af88ff700b19a4c2a428f5b89e7ad5f51f
Igor Shadurin <igor.shadurin@gmail.com>
2024-10-10 16:00:49 +0300

    refactor: add base and mode networks

---
+++ src/network-config/base-mainnet-config.ts
@@ -0,0 +1,26 @@
+import { INetworkConfig } from './INetworkConfig'
+
+export const baseMainnetConfig: INetworkConfig = {
+  networkName: 'Base Mainnet',
+  chainId: 8453,
+  accountFactoryAddress: '0x00004EC70002a32400f8ae005A26081065620D20',
+  /**
+   * https://docs.alchemy.com/reference/eth-supportedentrypoints (v0.6.0)
+   */
+  entryPointAddress: '0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789',
+  /**
+   * Create2 Address of Social Connections contract
+   */
+  socialConnectionsAddress: '0xB7C1C10A71d3C90f42351bec7E4BCd647C992743',
+  /**
+   * Create2 Address of Filesystem Changes contract
+   */
+  filesystemChangesAddress: '0x55043C8f3e8Ec55D2d60Acef83024F3b6da5AAf0',
+  appAuthUrl: 'https://dappy.in',
+  rpcUserOperationsUrl: 'https://base-mainnet.g.alchemy.com/v2/ublj-ZfkDCQxMSsTq-SWrGfHrfvGY58d',
+  userOperationsExplorerUrl: '',
+  simpleExplorerUrl: 'https://base.blockscout.com',
+  verificationRpcUrl: 'https://verify.dappykit.org',
+  farcasterAuthFrameUrl: 'https://farcaster-auth-frame.dappykit.org',
+  farcasterAuthApiUrl: 'https://farcaster-auth-api.dappykit.org',
+}
--- src/network-config/index.ts
+++ src/network-config/index.ts
@@ -1,5 +1,17 @@
 import { optimismSepoliaConfig } from './optimism-sepolia-config'
 import { optimismMainnetConfig } from './optimism-mainnet-config'
+import { baseSepoliaConfig } from './base-sepolia-config'
+import { baseMainnetConfig } from './base-mainnet-config'
+import { modeMainnetConfig } from './mode-mainnet-config'
+import { modeSepoliaConfig } from './mode-sepolia-config'
 import { INetworkConfig } from './INetworkConfig'
 
-export { optimismSepoliaConfig, optimismMainnetConfig, INetworkConfig }
+export {
+  optimismSepoliaConfig,
+  optimismMainnetConfig,
+  baseSepoliaConfig,
+  baseMainnetConfig,
+  modeMainnetConfig,
+  modeSepoliaConfig,
+  INetworkConfig,
+}
