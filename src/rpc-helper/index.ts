a9b5a0de6a44ad5be08e65b4f718849d6a5c9f29
Igor Shadurin <haletskaya@gmail.com>
2024-12-05 03:37:34 -0800

    refactor: permissionless updated, entry point updated to v7 (#53)

--- src/rpc-helper/index.ts
+++ src/rpc-helper/index.ts
@@ -1,16 +1,15 @@
 import { INetworkConfig } from '../network-config'
 import { HDAccount } from 'viem/accounts'
 import { Chain, createPublicClient, http, HttpTransport } from 'viem'
-import { LightSmartAccount, signerToLightSmartAccount } from 'permissionless/accounts'
-import { createSmartAccountClient, ENTRYPOINT_ADDRESS_V06, SmartAccountClient } from 'permissionless'
-import { ENTRYPOINT_ADDRESS_V06_TYPE } from 'permissionless/types'
+import { toSimpleSmartAccount, ToSimpleSmartAccountReturnType } from 'permissionless/accounts'
+import { createSmartAccountClient, SmartAccountClient } from 'permissionless'
 import { getChain } from '../utils/chain'
 import { assertNotEmptySigner } from '../connections/utils'
+import { entryPoint07Address, type SmartAccount } from 'viem/account-abstraction'
 
-export type LightSmartAccountType = LightSmartAccount<ENTRYPOINT_ADDRESS_V06_TYPE, HttpTransport, Chain>
+export type LightSmartAccountType = ToSimpleSmartAccountReturnType
 
-// prettier-ignore
-export type SmartAccountClientType = SmartAccountClient<ENTRYPOINT_ADDRESS_V06_TYPE, HttpTransport, Chain, LightSmartAccountType>
+export type SmartAccountClientType = SmartAccountClient<HttpTransport, Chain, SmartAccount>
 
 export class RpcHelper {
   private account: LightSmartAccountType | undefined
@@ -38,10 +37,13 @@ export class RpcHelper {
     assertNotEmptySigner(this.eoaSigner)
 
     if (!this.account) {
-      this.account = await signerToLightSmartAccount(this.getPublicClient(), {
-        signer: this.eoaSigner,
-        entryPoint: ENTRYPOINT_ADDRESS_V06,
-        lightAccountVersion: '1.1.0',
+      this.account = await toSimpleSmartAccount({
+        owner: this.eoaSigner,
+        client: this.getPublicClient(),
+        entryPoint: {
+          address: entryPoint07Address,
+          version: '0.7',
+        },
       })
     }
 
@@ -56,7 +58,6 @@ export class RpcHelper {
       const account = await this.getAccount()
       this.accountClient = createSmartAccountClient({
         account,
-        entryPoint: ENTRYPOINT_ADDRESS_V06,
         chain: getChain(this.config.chainId),
         bundlerTransport: http(this.config.rpcUserOperationsUrl),
         // middleware: {
